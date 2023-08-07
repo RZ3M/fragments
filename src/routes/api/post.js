2629cc98170f3a436c1e38608d09508ae3ebe1e7
Jack <omnomnomny@gmail.com>
2023-08-07 19:40:25 -0400

    added more detailed error message

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -20,6 +20,8 @@ module.exports = async (req, res) => {
     res.set({ Location: location }).status(201).json(response.createSuccessResponse({ fragment }));
     logger.info({ fragment: fragment }, `Fragment have been posted successfully`);
   } catch (error) {
-    res.status(404).json(response.createErrorResponse(404, 'Unable to POST the fragment'));
+    res
+      .status(404)
+      .json(response.createErrorResponse(404, 'Unable to POST the fragment: ' + error.message));
   }
 };
